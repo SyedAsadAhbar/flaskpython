e58b4c8f91a0a62e11651ad324589ce78dac03d6
syedasad <155992793+SyedAsadAhbar@users.noreply.github.com>
2025-03-08 12:44:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is a Dockerized Flask app deployed on Amazon ECS."
+    return "Hello, World111! This is a Dockerized Flask app deployed on Amazon ECS."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
